96e1cffbc88816ba4526de3ad7054b6ee52d7b5c
arap <conwenans@gmail.com>
2013-12-10 01:03:33 +0100

    new ways of collision detection.
    separte quadtrees for bullets from ships and towers.

--- ChipTotality/src/com/screen/GameStage.java
+++ ChipTotality/src/com/screen/GameStage.java
@@ -8,8 +8,11 @@ import java.util.Vector;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.utils.Array;
 import com.main.ChipTotality;
 import com.main.Settings;
 import com.res.Sounds;
@@ -18,53 +21,72 @@ import com.world.building.Building;
 import com.world.ship.Ship;
 import com.world.ship.TestShip1;
 import com.world.tower.Bullet;
+import com.world.tower.MoveToAtConstSpeed;
 import com.world.tower.TestBullet1;
 //class for ships, bullets, and buildings
 
 public class GameStage extends Stage{
 	final ChipTotality game;
 
-	
-	public static Vector<Ship> ships;
+	//statki moga zderzac sie z pociskami wyslanymi przez wiezyczke
+	public static Vector<Ship> ships = new Vector<Ship>();
+	//pociski wyslane rpzez staki moga zderzac sie z budynkami
+	public static Vector<Building> buildings= new Vector<Building>();
 
-	QuadTree<Actor> quadTree; 
-	QuadTree<Bullet> bulletTreeTower, bulletTreeShip;	
-	ArrayList<Bullet> listOfBulletTower, listOfBulletShip;	// Bullets from towers, bullets from ships
+	public QuadTree<Actor> quadTree; 
+	public static QuadTree<Bullet> bulletsFromTowersTree, bulletsFromShipsTree;	
+	public static Array<Bullet> bulletsFromTowers, bulletsFromShips;	// Bullets from towers, bullets from ships
 	//ArrayList<Explosion> listOfExplossions;	
 
 	
-	ShipManager shipMgr;
+	ShipManager shipManager;
 	public GameStage(ChipTotality game) {
 		super(Settings.WIDTH, Settings.HEIGHT, true, game.batch);
 		this.game=game;
 		quadTree = new QuadTree<Actor>(0, new Rectangle(0,0, Settings.tilesHorizontal*Settings.tileSize, Settings.tilesVertical*Settings.tileSize));
-		bulletTreeTower = new QuadTree<Bullet>(0, new Rectangle(0,0, Settings.tilesHorizontal*Settings.tileSize, Settings.tilesVertical*Settings.tileSize));
-		bulletTreeShip = new QuadTree<Bullet>(0, new Rectangle(0,0, Settings.tilesHorizontal*Settings.tileSize, Settings.tilesVertical*Settings.tileSize));
-		shipMgr = new ShipManager(this);
+		bulletsFromShipsTree = new QuadTree<Bullet>(0, new Rectangle(0,0, Settings.tilesHorizontal*Settings.tileSize, Settings.tilesVertical*Settings.tileSize));
+		bulletsFromTowersTree = new QuadTree<Bullet>(0, new Rectangle(0,0, Settings.tilesHorizontal*Settings.tileSize, Settings.tilesVertical*Settings.tileSize));
+		bulletsFromShips = new Array<Bullet>();
+		bulletsFromTowers = new Array<Bullet>();
+		shipManager = new ShipManager(this);
+		
 		
 		//TODO usunac po testach
 		TestShip1 ts1 = new TestShip1(300, 300);
 		addActor(ts1);
-		ts1.action.setDuration(10);
-		ts1.action.setInterpolation(Interpolation.exp10);
-		ts1.action.setPosition(1500, 1400);	
-		ts1.addAction(ts1.action);	
+		MoveToAction action = new MoveToAction();
+		action.setDuration(10);
+		action.setInterpolation(Interpolation.bounceOut);
+		action.setPosition(1300, 1200);	
+		ts1.addAction(action);	
+		ships.add(ts1);
 		
 		TestShip1 ts2 = new TestShip1(590, 300);
+		MoveToAtConstSpeed mta = new MoveToAtConstSpeed(new Vector2(800, 500), 2);
+		ts2.addAction(mta);	
 		addActor(ts2);
+		ships.add(ts2);
+		
 		TestShip1 ts3 = new TestShip1(1500, 1400);
 		addActor(ts3);
+		ships.add(ts3);
 		
 		TestBullet1 tb1 = new TestBullet1(1200, 230, ts1);
 		TestBullet1 tb2 = new TestBullet1(1000, 230, ts1);
 		TestBullet1 tb3 = new TestBullet1(800, 130, ts1);
 		TestBullet1 tb4 = new TestBullet1(-400, 530, ts1);
 		TestBullet1 tb5 = new TestBullet1(0, 0, ts1);
+		bulletsFromTowers.add(tb1);
+		bulletsFromTowers.add(tb2);
+		bulletsFromTowers.add(tb3);
+		bulletsFromTowers.add(tb4);
+		bulletsFromTowers.add(tb5);
 		addActor(tb1);
 		addActor(tb2);
 		addActor(tb3);
 		addActor(tb4);
 		addActor(tb5);
+		
 		
 		///////////////////
 		setCamera(game.camera);		
@@ -81,8 +103,8 @@ public class GameStage extends Stage{
 			Actor currentActor = i.next(); 
 			returnObjects.clear();
 			returnObjects=quadTree.retrieve(currentActor);
-			for (Actor possiblyColliding : returnObjects) {
-				Rectangle currBounds = new Rectangle(currentActor.getX(), currentActor.getY(), currentActor.getWidth(), currentActor.getHeight());
+			Rectangle currBounds = new Rectangle(currentActor.getX(), currentActor.getY(), currentActor.getWidth(), currentActor.getHeight());
+			for (Actor possiblyColliding : returnObjects) {				
 				Rectangle possBounds = new Rectangle(possiblyColliding.getX(), possiblyColliding.getY(), possiblyColliding.getWidth(), possiblyColliding.getHeight());
 				if(currBounds.overlaps(possBounds)){								
 					Gdx.app.log("collision", "collision beetween"+currentActor.getClass().getSimpleName()+" and "+ possiblyColliding.getClass().getSimpleName());
@@ -96,77 +118,49 @@ public class GameStage extends Stage{
 				
 			}		   		   
 		}
-		/*for (Actor currentActor : getActors()) {
+	}
+	
+	public void checkBulletCollisionsWithBuildings(){
+		bulletsFromShipsTree.clear();
+		bulletsFromShipsTree.insert(bulletsFromShips);
+		ArrayList<Bullet> returnObjects = new ArrayList<Bullet>();
+		
+		for (Building building : buildings) {			
 			returnObjects.clear();
-			returnObjects=quadTree.retrieve(currentActor);
-			for (Actor possiblyColliding : returnObjects) {
-				Rectangle currBounds = new Rectangle(currentActor.getX(), currentActor.getY(), currentActor.getWidth(), currentActor.getHeight());
+			returnObjects=bulletsFromShipsTree.retrieve(building);
+			Rectangle currBounds = new Rectangle(building.getX(), building.getY(), building.getWidth(), building.getHeight());		
+			for (Bullet possiblyColliding : returnObjects) {
 				Rectangle possBounds = new Rectangle(possiblyColliding.getX(), possiblyColliding.getY(), possiblyColliding.getWidth(), possiblyColliding.getHeight());
-				if(currBounds.overlaps(possBounds))								
-					Gdx.app.log("collision", "collision beetween"+currentActor.getClass().getSimpleName()+" and "+ possiblyColliding.getClass().getSimpleName());													
-			}
-		}*/
-	}
-	
-	public void checkBulletCollisions(Vector<Building> buildings){
-		for(Building building : buildings)
-		{
-			ArrayList<Bullet> bulls = bulletTreeShip.retrieve(building);
-			for(Bullet b : bulls)
-			{
-				if((new Rectangle(b.getX(), b.getY(), b.getWidth(), b.getHeight())).overlaps(new Rectangle(building.getX(),  building.getY(),  building.getWidth(),  building.getHeight())))  
-				{					
-					//	building.health-=b.damage;
-					//TODO Jakas animacja wybuchu
-					bulls.remove(b);
-					b.remove();
+				if(currBounds.overlaps(possBounds)){
+					Sounds.play("BoxCrash");
+					possiblyColliding.remove();
+					bulletsFromShips.removeValue(possiblyColliding, true);
 				}
 			}
 		}
 	}
-	public void checkBulletCollisionsShips(Vector<Ship> ships) {
-		for(Ship ship : ships)	{
-			ArrayList<Bullet> bulls = bulletTreeTower.retrieve(ship);
-			for(Bullet b : bulls)
-			{
-				if((new Rectangle(b.getX(), b.getY(), b.getWidth(), b.getHeight())).overlaps(new Rectangle(ship.getX(),  ship.getY(),  ship.getWidth(),  ship.getHeight())))  
-				{
-					
-					//	ship.health-=b.damage;
-					//TODO Jakas animacja wybuchu
-					bulls.remove(b);
-					b.remove();
+	
+	
+	public void checkBulletCollisionsWithShips() {
+		bulletsFromTowersTree.clear();
+		bulletsFromTowersTree.insert(bulletsFromTowers);
+		ArrayList<Bullet> returnObjects = new ArrayList<Bullet>();
+		
+		for (Ship ship : ships) {
+			returnObjects.clear();
+			returnObjects=bulletsFromTowersTree.retrieve(ship);
+			Rectangle currBounds = new Rectangle(ship.getX(), ship.getY(), ship.getWidth(), ship.getHeight());		
+			for (Bullet possiblyColliding : returnObjects) {
+				Rectangle possBounds = new Rectangle(possiblyColliding.getX(), possiblyColliding.getY(), possiblyColliding.getWidth(), possiblyColliding.getHeight());
+				if(currBounds.overlaps(possBounds)){
+					Sounds.play("BoxCrash");
+					possiblyColliding.remove();
+					bulletsFromTowers.removeValue(possiblyColliding, true);
 				}
 			}
 		}
 	}
-	public void Update()
-	{
-		bulletTreeTower.clear();
-		bulletTreeShip.clear();
 	
-		for(Bullet b : listOfBulletTower) {			
-			bulletTreeTower.insert(b);
-		}
-			
-		for(Bullet b : listOfBulletShip) {
-			bulletTreeShip.insert(b);
-		}
-		final int licz = 0; // Liczni
-		
-		
-	}
-	public void AddBullet(Bullet  bull, boolean fromShip) {
-		if(fromShip)
-		{
-			listOfBulletShip.add(bull);
-		}
-		else
-		{
-			listOfBulletTower.add(bull);
-		}
-		addActor(bull);
-	}
 	
 
 	public Building getRandBuilding() {
