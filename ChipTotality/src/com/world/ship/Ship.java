1649b15f57d77098f0fb422053d02138988af469
arap <conwenans@gmail.com>
2013-12-23 20:24:38 +0100

    simple ai, capable of searching target and shooting

--- ChipTotality/src/com/world/ship/Ship.java
+++ ChipTotality/src/com/world/ship/Ship.java
@@ -3,40 +3,114 @@ package com.world.ship;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.gameInfo.Stats;
 import com.res.Loader.AssetsLoader;
+import com.screen.GameStage;
+import com.world.building.Building;
 
 public abstract class Ship extends Actor{
-	private float lastShoot;
-	public int hitpoints;
-	private Sprite sprite;
-	public Actor currentTarget;
+	public enum CurrentAction{
+		search,
+		approach,
+		shoot;
+	}
 	
+	protected float lastShoot;
+	public int hitpoints;
+	private final Sprite sprite;
+	protected Building currentTarget;
+	Stats.Bullets bulletType;
+	CurrentAction currentAction;
 	
 	Ship(int x, int y){
 		setBounds(x, y, getStats().width, getStats().height);
-		this.hitpoints=getStats().maxHitpoints;
-		
-		addListener(new InputListener(){
-			//TODO zrobic jakis sensowny element GUI, ktory by po kliknieciu na statek sie pojawial i dawal jakies info  o nim
-			@Override
-			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
-                System.out.println("down");
-                return true;
-        }
-		});
-		
-		sprite = new Sprite(AssetsLoader.getTexture(this.getClass().getSimpleName()));				
+		this.hitpoints=getStats().maxHitpoints;		
+		sprite = new Sprite(AssetsLoader.getTexture(this.getClass().getSimpleName()));	
+		bulletType=Stats.Bullets.simpleBullet;
+		currentAction = CurrentAction.search;
 	}
 	
-	public abstract void shoot();
+	@Override
+	public void act(float delta){
+		super.act(delta);
+		
+		lastShoot+=delta;
+		if(currentAction==CurrentAction.shoot && lastShoot>getStats().shootDelay){
+			if(targetInRange()){
+				shoot();
+				lastShoot=0;
+			}
+			else{
+				currentAction=CurrentAction.search;
+			}
+		}
+		else if(currentAction==CurrentAction.approach){
+			approach(getStats().range);
+		}
+		else if(currentAction==CurrentAction.search)
+			searchClosestTarget();
+		
+	}
 	
-	public void destroy(){
-		//TODO jakas eksplozja
-		remove();
+		
+	public abstract void shoot();
+
+	//looks for building to attack
+	public void searchClosestTarget(){
+		float shortestDistance=Float.POSITIVE_INFINITY;
+		Vector2 shipsCoords = new Vector2(getX(), getY());
+		
+		for (Actor building : GameStage.buildingsGroup.getChildren()) {
+			float distance = shipsCoords.dst(building.getX(), building.getY());
+			if(distance < shortestDistance){
+				shortestDistance=distance;
+				currentTarget=(Building)building;			
+			}
+			//target in range - there's no need to look for another one
+			if(shortestDistance<getStats().range){
+				currentAction=CurrentAction.shoot;
+				return;
+			}
+			
+		}
+
+		setCurrentAction(Ship.CurrentAction.approach);
+	}
+	
+	
+	//approach building which was previously searched
+	public void approach(float distance){
+		//required when there is no buildings in the game
+		if(currentTarget==null){
+			currentAction=CurrentAction.search;
+			return;
+		}
+		
+		Vector2 direction = new Vector2(currentTarget.getX()-getX(), currentTarget.getY()-getY());
+		direction.nor();
+		Vector2 targetCoords = new Vector2(currentTarget.getX(), currentTarget.getY());
+		
+		if(targetCoords.dst(getX(), getY())<distance){
+			setCurrentAction(Ship.CurrentAction.shoot);
+			return;
+		}
+		
+		//building was destroyed
+		if(currentTarget.hasParent()!=true){
+			setCurrentAction(Ship.CurrentAction.search);
+			return;
+		}
+					
+		setX(getX()+direction.x*getStats().speed);
+		setY(getY()+direction.y*getStats().speed);	
+	}
+	
+	private boolean targetInRange() {
+		Vector2 coordsFloat = new Vector2(getX(), getY());
+		//target ship has no parent - he formally doesn't exist in the world
+		return (currentTarget!=null &&currentTarget.hasParent() && coordsFloat.dst(currentTarget.getX(), currentTarget.getY()) <=getStats().range);
 	}
 	
 	@Override
@@ -46,7 +120,19 @@ public abstract class Ship extends Actor{
 		sprite.draw(batch);
 	}
 	
+	public void destroy(){
+		remove();
+	}
+	
 	public Stats.Ships getStats(){
 		return Stats.Ships.valueOf(this.getClass().getSimpleName());
 	}
+	
+	public void setCurrentAction(CurrentAction action){
+		this.currentAction=action;
+	}
+	public void setCurrentTarget(Building target){
+		currentTarget=target;
+	}
+	
 }
--- ChipTotality/src/com/world/ship/TestShip1.java
+++ ChipTotality/src/com/world/ship/TestShip1.java
@@ -2,16 +2,13 @@ package com.world.ship;
 
 import com.action.MoveTowardsAction;
 import com.badlogic.gdx.math.Vector2;
-import com.res.Loader.AssetsLoader;
 import com.screen.GameStage;
 import com.world.tower.Bullet;
 
 
 
 public class TestShip1 extends Ship {
-	static int firepower=50;
-
-
+	
 	
 	public TestShip1(int x, int y)  {
 		super(x, y);
@@ -20,11 +17,9 @@ public class TestShip1 extends Ship {
 
 	@Override
 	public void shoot() {
-		Bullet newBullet = new Bullet(getX(), getY(), 10, 10, AssetsLoader.getTexture("TestBullet1"), firepower);
-		newBullet.addAction(new MoveTowardsAction(new Vector2(currentTarget.getX()-getX(), currentTarget.getY()-getY()), 0.02f, 3, true));
+		Bullet newBullet = bulletType.spawnBullet(getX(), getY());
+		newBullet.addAction(new MoveTowardsAction(new Vector2(currentTarget.getX()-getX(), currentTarget.getY()-getY()), 0.02f, 3, true));	
 		GameStage.bulletsFromShipsGroup.addActor(newBullet);
-		
 	}
 	
-
 }
